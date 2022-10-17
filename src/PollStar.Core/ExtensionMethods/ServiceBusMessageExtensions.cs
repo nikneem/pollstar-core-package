aee7151148abf837231e2f12c77cb79075416905
Eduard <e.keilholz@4dotnet.nl>
2022-10-17 16:28:29 +0200

    Reformat messages

--- src/PollStar.Core/ExtensionMethods/ServiceBusMessageExtensions.cs
+++ src/PollStar.Core/ExtensionMethods/ServiceBusMessageExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace PollStar.Core.ExtensionMethods;
 
@@ -8,7 +9,7 @@ public static class ServiceBusMessageExtensions
     public static ServiceBusMessage ToServiceBusMessage(this object payload, string? correlationId = null)
     {
         var payloadJson = JsonConvert.SerializeObject(payload);
-        var message = new ServiceBusMessage(payloadJson)
+        var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(payloadJson))
         {
             CorrelationId = correlationId ?? Guid.NewGuid().ToString()
         };
