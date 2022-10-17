5d109757128b80893c4ee4f388d362458744bc3c
Eduard <e.keilholz@4dotnet.nl>
2022-10-17 16:25:57 +0200

    Adding service bus message formatter

---
+++ src/PollStar.Core/ExtensionMethods/ServiceBusMessageExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+
+namespace PollStar.Core.ExtensionMethods;
+
+public static class ServiceBusMessageExtensions
+{
+    public static ServiceBusMessage ToServiceBusMessage(this object payload, string? correlationId = null)
+    {
+        var payloadJson = JsonConvert.SerializeObject(payload);
+        var message = new ServiceBusMessage(payloadJson)
+        {
+            CorrelationId = correlationId ?? Guid.NewGuid().ToString()
+        };
+        return message;
+    }
+    
+}
