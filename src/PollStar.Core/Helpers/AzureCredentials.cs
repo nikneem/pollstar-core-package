9eb183dad3b55706697552a09528ea9897ada760
Eduard Keilholz <e.keilholz@4dotnet.nl>
2023-05-09 09:16:01 +0200

    Adding standard azure credentials

---
+++ src/PollStar.Core/Helpers/AzureCredentials.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Core;
+using Azure.Identity;
+
+namespace PollStar.Core.Helpers;
+
+public static class AzureCredentials
+{
+    public static TokenCredential GetAzureCredential()
+    {
+        return new ChainedTokenCredential(
+            new ManagedIdentityCredential(),
+            new AzureCliCredential(),
+            new VisualStudioCredential(),
+            new VisualStudioCodeCredential());
+    }
+}
